0298d7478efe4b661338995636f9e63e9169f1f8
victorb-s <victorb.santos15@gmail.com>
2024-10-26 12:55:01 -0300

    Finalização da Section Início, Header e Footer
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
 import Header from "../../components/Header"
-
+import Footer from "../../components/Footer"
+import SectionInicio from "../../components/HomeComponents/SectionInicio"
 const Home = () => {
   return (<>
     <Header/>
+      <SectionInicio/>
+    <Footer/>
   </>)
 }
 
