a78cf1af3f5ffd67aa5e51e99059757619ade60b
Arthur Tavares <tutstavares@Outlook.com>
2024-10-27 20:38:47 -0300

    Section projetos recentes

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,16 +1,20 @@
-import Header from "../../components/Header"
-import SectionHeading from "../../components/HomeComponents/SectionHeading"
-import ProjectNavigation from "../../components/HomeComponents/ProjectNavigation"
-import {SectionContainer} from './styles'
+import Header from "../../components/Header";
+import SectionHeading from "../../components/HomeComponents/SectionHeading";
+import ProjectNavigation from "../../components/HomeComponents/ProjectNavigation";
+import { SectionContainer } from "./styles";
+import SectionProjetos from "../../components/HomeComponents/SectionProjetos";
 
 const Home = () => {
-  return (<>
-    <Header/>
-    <SectionContainer >
-        <SectionHeading text="Projetos Recentes"/>
-        <ProjectNavigation/>
-    </SectionContainer>
-  </>)
-}
+  return (
+    <>
+      <Header />
+      <SectionContainer>
+        <SectionHeading text="Projetos Recentes" />
+        <ProjectNavigation />
+        <SectionProjetos></SectionProjetos>
+      </SectionContainer>
+    </>
+  );
+};
 
-export default Home
+export default Home;
