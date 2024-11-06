eb2348f3a89ad446d0b8872a5d13559b06d7a215
Victor Barbosa <114593367+victorb-s@users.noreply.github.com>
2024-11-06 17:40:52 -0300

    Revert "feat: tela de redação"

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,30 +1,9 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 
-import SectionInicio from "../../components/HomeComponents/SectionInicio";
-import TrendingTopics from "../../components/HomeComponents/TrendingTopics";
-import { HomeInfos } from "../../components/HomeComponents/HomeSecInfos";
-
-import { SectionContainer } from "./styles";
-import SectionHeading from "../../components/HomeComponents/SectionHeading";
-import ProjectNavigation from "../../components/HomeComponents/ProjectNavigation";
-import SectionProjetos from "../../components/HomeComponents/SectionProjetos";
 
 const Home = () => {
   return (
-    <>
-      <Header />
-      <SectionInicio />
-      <TrendingTopics />
-      <HomeInfos />
-      <SectionContainer>
-        <SectionHeading text="Projetos Recentes" />
-        <ProjectNavigation />
-        <SectionProjetos  />
-      </SectionContainer>
-      <Footer />
-    </>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
