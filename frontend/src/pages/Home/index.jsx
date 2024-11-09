b02f7f5814e3ed915d049b6fc8f924ecda0ad8f6
cauaojordao <cauaojordao@gmail.com>
2024-11-08 22:16:49 -0300

    Reponsividade da section projetos recentes implementada

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -7,7 +7,6 @@ import { HomeInfos } from "../../components/HomeComponents/HomeSecInfos";
 
 import { SectionContainer } from "./styles";
 import SectionHeading from "../../components/HomeComponents/SectionHeading";
-import ProjectNavigation from "../../components/HomeComponents/ProjectNavigation";
 import SectionProjetos from "../../components/HomeComponents/SectionProjetos";
 
 const Home = () => {
@@ -19,7 +18,6 @@ const Home = () => {
       <HomeInfos />
       <SectionContainer>
         <SectionHeading text="Projetos Recentes" />
-        <ProjectNavigation />
         <SectionProjetos  />
       </SectionContainer>
       <Footer />
