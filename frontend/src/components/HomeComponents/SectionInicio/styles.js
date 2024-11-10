7d0650c0d409a1e540a7e015bcea44e4ae409c80
victorb-s <victorb.santos15@gmail.com>
2024-11-09 22:11:30 -0300

    Adicionar responsividade na section inicio - home
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- frontend/src/components/HomeComponents/SectionInicio/styles.js
+++ frontend/src/components/HomeComponents/SectionInicio/styles.js
@@ -1,8 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import RainbowSide from '../../../assets/JCRainbow.svg';
+
+const ResponsiveContainer = css`
+    @media (max-width: 1024px) {
+        background: none;
+        height: 70vh;
+    }
+
+    @media (max-width: 768px) {
+        height: 50vh;
+    }
+`
 
 export const Container = styled.div`
     height: 90vh;
@@ -16,11 +27,27 @@ export const Container = styled.div`
     background-repeat: no-repeat;
     background-position: right;
     background-size: contain;
+
+    ${ResponsiveContainer}
+`
+
+const ResponsiveColumn = css`
+    @media (max-width: 1024px) {
+        max-width: 60%;
+    }
+
+    @media (max-width: 768px) {
+        max-width: 75%;
+    }
+
+    @media (max-width: 480px) {
+        max-width: 100%;
+    }
 `
 
 export const Column = styled.div`
     height: 100%;
-    width: 100%;
+    width: 100vw;
     max-width: 42%;
 
     padding-top: 10vh;
@@ -29,7 +56,9 @@ export const Column = styled.div`
     display: flex;
     flex-direction: column;
 
+    ${ResponsiveColumn}
 `
+
 
 export const TextContainer = styled.div`
     display: flex;
@@ -38,14 +67,29 @@ export const TextContainer = styled.div`
 
     h1{
         color: #DA251C;
-        font-size: 64px;
+        font-size: clamp(2rem, 4vw, 4rem);
     }
 
     p{
         padding-top: 3vh;
-        font-size: 18px;
+        font-size: clamp(0.8rem, 2vw, 1.1rem);
         text-align: justify;
         font-weight: 300;
+    }
+`
+
+const ResponsiveBtnContainer = css`
+    @media (max-width: 1024px) {
+        width: 20vw;
+    }
+
+    @media (max-width: 768px) {
+        width: 25vw;
+    }
+
+    @media (max-width: 480px) {
+        width: 35vw;
+        height: 4vh;
     }
 `
 
@@ -65,14 +109,25 @@ export const ButtonContainer = styled(Link)`
     text-decoration: none;
     transition: 0.2s ease-in-out;
 
+    ${ResponsiveBtnContainer}
+
     p{
         color: #034C8C;
+        font-size: clamp(0.7rem, 2vw, 1rem);
         padding-left: 1vw;
     }
 
     &:hover{
         transform: scale(1.02);
     }
+`
+
+const ResponsiveAC = css`
+    @media (max-width: 1024px) {
+        width: 20%;
+    }
+
+
 `
 
 export const ArrowCircle = styled.div`
@@ -85,12 +140,14 @@ export const ArrowCircle = styled.div`
 
     width: 16%;
     height: 100%;
+
+    ${ResponsiveAC}
 `
 
 export const IconArrow = styled(FontAwesomeIcon)`
     color: #034C8C;
     font-weight: bold;
-    font-size: 20px;
+    font-size: clamp(0.8rem, 2vw, 1.2rem);
 `
 
 // export const ImageSide = styled.img`
